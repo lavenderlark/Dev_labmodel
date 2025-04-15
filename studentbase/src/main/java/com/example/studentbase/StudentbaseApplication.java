78fb6ce4d0445e13b8e373eee759f29b8dfdb43a
Miraclin <157980372+lavenderlark@users.noreply.github.com>
2025-04-15 12:08:45 +0530

    Add files via upload

---
+++ studentbase/src/main/java/com/example/studentbase/StudentbaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentbaseApplication.class, args);
+	}
+
+}
